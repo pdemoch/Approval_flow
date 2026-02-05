ce9036f6b20c15e24db2d29e554cb076ca743203
Phillipe Democh da Silva <phillipe.silva@lineaalimentos.com.br>
2026-02-05 11:13:46 -0300

    Setup inicial do projeto Streamlit

---
+++ app.py
@@ -0,0 +1,6 @@
+import streamlit as st
+
+st.set_page_config(page_title="Portal de Aprovação", layout="wide")
+
+st.title("🚚 Portal de Aprovação de Custos Extras")
+st.write("Deploy funcionando corretamente 🎉")
